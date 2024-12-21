a919bd7e3a6bc97d8522ffa0796a6d7403d4f702
Himanshu kumar <himanshukr1505892@gmail.com>
2024-12-21 04:52:08 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+import Products from './Products';
+
+function App() {
+  var [a,b] = useState(69);
+  return (
+    <div className='w-full h-screen bg-zinc-900 text-white p-5'>
+      <Products age='25' data={{age: 25, name:'harsh'}}/>
+    </div> 
+  )
+}
+
+export default App
